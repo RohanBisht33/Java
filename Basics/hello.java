a1146e29a87061425b7292fb342bd4e3d699d8e1
RohanBisht33 <bishtrohan33@gmail.com>
2026-02-20 17:59:45 +0530

    Hello World

---
+++ Basics/hello.java
@@ -0,0 +1,31 @@
+//This is a comment
+//Basic Hello World printing Java code 
+//----------------------------------------------------------
+//Class name (Hello) is Capitalized form of filename (hello)
+//class is a keyword
+
+class Hello{
+    
+    //Compulsory (main) function/method. It is called by JVM (Java Virtual Machine)
+    //(public) is access specifier, (static) is class variable, (void) is return type, (main) is method name, (String []args) is parameter
+    //------------------------------------------------------------------------------------------------------------------------------------
+    public static void main(String args[]){
+        //System.out.println() is used to show output on screen
+        //-----------------------------------------------------------------------
+        //(System) is a class name
+        //(out) is a static variable present in System class of type PrintStream
+        //(println) is a method present in PrintStream class
+        //-----------------------------------------------------------------------
+        System.out.println("Hello World");
+    }
+}
+
+//Compile it using:
+// javac (filename.java)
+//----------------------
+//Run it using:
+// java (classname)
+
+//For example
+// javac hello.java
+// java Hello
